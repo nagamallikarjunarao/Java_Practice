4d9b0b4c32c3f844542e15a79e4dee4854084ce0
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-20 21:04:30 +0530

    Add files via upload

---
+++ Maximum_two_num.java
@@ -0,0 +1,10 @@
+public class Maximum_two_num   
+{  
+    public static void main(String args[])  
+    {  
+        int x = 20;  
+        int y = 50;  
+        //print the maximum of two numbers   
+        System.out.println(Math.max (x, y));  
+    }  
+}  
