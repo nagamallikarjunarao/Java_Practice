0918b2af84ecf2fd1988dd0585186ea6ddb1e650
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-30 12:16:17 +0530

    Add files via upload

---
+++ Patternalphabet.java
@@ -0,0 +1,14 @@
+class Patternalphabet{
+  public static void main (String args[]){
+    int i,j;
+    int alp=65;
+    for (i=0;i<=5;i++){
+      
+    
+    for(j=0;j<=i;j++){
+     System.out.print((char) (alp +j)+" "  );
+    }
+      System.out.println( );
+    }
+  }
+}
