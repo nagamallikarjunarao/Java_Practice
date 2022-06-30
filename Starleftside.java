02ba297221514bc2a357cd0333082abb28a3dc4c
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-06-30 12:12:57 +0530

    Add files via upload

---
+++ Starleftside.java
@@ -0,0 +1,13 @@
+class Starleftside{
+  public static void main (String args[]){
+    int i,j;
+    for (i=6-1;i>=1;i--){
+      
+    
+    for(j=1;j<=i;j++){
+     System.out.print("*"+" ");
+    }
+      System.out.println( );
+    }
+  }
+}
