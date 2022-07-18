953cd6937ef1ed420785112122f451e14a11bc6b
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-18 12:02:11 +0530

    Add files via upload

---
+++ Lenth.java
@@ -0,0 +1,31 @@
+//Write a JAVA program to find length of a string and compare and concatenate two     strings.
+
+class Lenth{
+  public static void main(String args[])
+    {
+      String s1="Welcome" ;
+     String s2= s1.concat(" nani");//Welcome 
+      System.out.println(s2.length());
+      System.out.println(s1.compareTo(s2));
+      System.out.println(s1);
+      System.out.println(s2);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
