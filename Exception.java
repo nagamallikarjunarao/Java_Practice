ecfe720e7da0c1e15fa099a1ce354e0e256a14ce
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-27 15:06:56 +0530

    Add files via upload

---
+++ Exception.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class InvalidMobilenumberException extends Exception
+  {
+    public InvalidMobilenumberException(String msg)
+    {
+      super(msg);
+    }
+  }
+ class Exception
+  {
+    public  static void validate(String mobilenumber) throws InvalidMobilenumberException
+    {
+      if(mobilenumber.length()<10)
+      {
+        throw new InvalidMobilenumberException("invalid mobile number");
+      }
+      else{
+        System.out.println(mobilenumber);
+      }
+    }
+    public static void main(String args[])
+    {
+      try{
+        validate("1234567756");
+      }
+      catch(InvalidMobilenumberException ex)
+        {
+          System.out.println(ex);
+        }
+    }
+    
+  }
+
+    
