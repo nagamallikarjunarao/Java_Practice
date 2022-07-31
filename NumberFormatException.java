6af7c63b17e7b6eac48c4282a3241cba83441592
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-31 19:38:01 +0530

    Add files via upload

---
+++ NumberFormatException.java
@@ -0,0 +1,22 @@
+/* 2. Write a program which will illustrate NumberFormatException. Create a test method, with in the test method accept two numbers 
+   using next() method of scanner class. Convert into numerical values and print addition of two numbers. 
+   Please write your comments regarding NumberFormatException such as "When NumberFormatException will occur in this program?"
+*/
+import java.util.Scanner;
+public class NumberFormatException {
+
+    public static void main(String args[]) {
+
+    Scanner in = new Scanner(System.in);
+   
+  System.out.print("Input first number: ");
+  int num1 = in.nextInt();
+   
+  System.out.print("Input second number: ");
+  int num2 = in.nextInt();
+   
+ 
+  System.out.println(num1 + " + " + num2 + " = " + 
+  (num1 + num2));
+    }
+}
