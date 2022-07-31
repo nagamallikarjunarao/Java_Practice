d2f8e76f231d16968870ed19c1b7c0dc1cb325f8
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-31 19:43:00 +0530

    Add files via upload

---
+++ MapHash.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class MapHash
+  {
+    public static void main(String args[])
+  {
+  Map<Integer,String> map=new HashMap<Integer,String>();
+    map.put(12,"Mango");
+    map.put(78,"Apple");
+    map.put(45,"Orange");
+    map.put(96,"Apple");
+    System.out.println(map);  
+  }
+  }
