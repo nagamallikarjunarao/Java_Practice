7722a5b08134b8e8d13e68e3ec52ecfa686a41f2
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-31 19:41:20 +0530

    Add files via upload

---
+++ ArithmeticException.java
@@ -0,0 +1,30 @@
+/* 1. Write a program to accept two numbers, one Arithmetic operator such as +, -, / , *  and print output of Arithmetic operation. 
+   Write switch case to perform each operation. Handle ArithmeticException when you are performing division operation. 
+*/
+import java.util.Scanner;
+public class ArithmeticException {
+
+    public static void main(String args[]) {
+ Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter any two positive integer numbers:");
+
+        // Reading data using readLine
+        int p = in.nextInt();
+        int q = in.nextInt();
+        int sum, sub, mul, mod;
+        float div;
+
+        sum = p + q;
+        sub = p - q;
+        mul = p * q;
+        div = p / q;
+        mod = p % q;
+
+        System.out.println("\nSUM         " + p + " + " + q + " = " + sum);
+        System.out.println("DIFFERENCE  " + p + " - " + q + " = " + sub);
+        System.out.println("PRODUCT     " + p + " * " + q + " = " + mul);
+        System.out.println("QUOTIENT    " + p + " / " + q + " = " + div);
+        System.out.println("MODULUS     " + p + " % " + q + " = " + mod);
+    }
+}
