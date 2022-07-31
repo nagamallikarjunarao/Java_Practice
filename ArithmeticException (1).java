da3e1a3139299b5f796198ad215fe6817c5c2eb4
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-31 19:38:48 +0530

    Add files via upload

---
+++ ArithmeticException (1).java
@@ -0,0 +1,24 @@
+/* 1. Write a program which will illustrate ArthmeticException. Create a parameterized method which can take two values and print division of two numbers. 
+Please write your comments regarding ArithmeticException such as "When ArithmeticException will occur in this program?"
+*/
+public class ArithmeticException {
+
+    public static void main(String args[]) {
+      int x =100, y = 0;
+int z;
+System.out.println("Hello world");
+try
+{
+z = x/y;
+System.out.println(z);
+}
+catch(Exception except)
+{
+System.out.println("Avoid dividing by integer 0" + except );
+}
+System.out.println("Hello class");
+System.out.println("Hello there");
+}
+}
+
+    
