caeaa98cfa7015110353e5bd3cc13bc43b5b0881
nagamallikarjunarao <106150056+nagamallikarjunarao@users.noreply.github.com>
2022-07-31 19:38:26 +0530

    Add files via upload

---
+++ ArrayIndexOutOfBoundsException (1).java
@@ -0,0 +1,15 @@
+/* 3. Write a program which will illustrate ArrayIndexOutOfBoundsException. 
+   Create an array variable with n size, accept values from user and store values into array, finally print all values on console. 
+   Please write your comments regarding ArrayIndexOutOfBoundsException such as " When ArrayIndexOutOfBoundsException occur in this program?"
+*/
+public class ArrayIndexOutOfBoundsException {
+
+    public static void main(String args[]) {
+    String[] arr = {"Rohit","Shikar","Virat","Dhoni"}; 
+      for(int i=0;i<=arr.length;i++) {       
+         System.out.println(arr[i]);      
+          
+        }  
+    }
+}
+
